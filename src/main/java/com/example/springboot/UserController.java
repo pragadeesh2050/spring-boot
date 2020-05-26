6eb3733fd1777e52065665af4bf0902d3296ecd2
pragadeesh2050 <pragadeesh2050@gmail.com>
2020-05-26 10:33:26 +0530

    adding apis

---
+++ src/main/java/com/example/springboot/UserController.java
@@ -0,0 +1,44 @@
+package com.example.springboot;
+
+
+import com.example.springboot.model.User;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+    private List<User> Users = new ArrayList<User>();
+
+    @GetMapping("/users")
+    public List<User> GetUsers(){
+        return this.Users;
+    }
+
+    @GetMapping("/users/{Id}")
+    public User GetSingleUser(@PathVariable("Id") int UserID){
+        return this.Users.get(UserID - 1);
+    }
+
+    @PostConstruct
+    private void LoadUsers(){
+        User user1 = new User();
+        user1.setId(1);
+        user1.setFirstName("Pragadeesh");
+        user1.setLastName("Chandrasekaran");
+        user1.setEmail("pragadeesh@gmail.com");
+        this.Users.add(user1);
+
+        User user2 = new User();
+        user2.setId(2);
+        user2.setFirstName("Aparna");
+        user2.setLastName("Pragadeesh");
+        user2.setEmail("pragadeesh@gmail.com");
+        this.Users.add(user2);
+    }
+}
---
+++ src/main/java/com/example/springboot/model/User.java
@@ -0,0 +1,50 @@
+package com.example.springboot.model;
+
+public class User {
+
+    private int Id;
+    private String FirstName;
+    private String LastName;
+    private String Email;
+    private String MiddleName;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getMiddleName() {
+        return MiddleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        MiddleName = middleName;
+    }
+}
